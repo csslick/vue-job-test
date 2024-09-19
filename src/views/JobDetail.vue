de0a0270db95bd36956818174773d52d9c5c790f
csslick <digitone@naver.com>
2024-09-19 18:34:59 +0900

    상세보기 - 지원하기 기능

--- src/views/JobDetail.vue
+++ src/views/JobDetail.vue
@@ -18,8 +18,10 @@
     </div>
     <!-- 하단 고정 버튼(작성자 글이 아닐 때만 노출) -->
     <div class="bottom-btn-group" v-if="!isMyPost">
-      <button class="btn-tel">전화문의</button>
-      <button class="btn-apply">지원하기</button>
+      <!-- <button class="btn-tel">전화문의</button> -->
+      <a class="btn-tel" :href="`tel:${post?.tel}`">전화문의</a>
+      <button class="btn-disable" v-if="isApplied">지원완료</button>
+      <button class="btn-apply" @click="handleApply" v-if="!isApplied">지원하기</button>
     </div>
     <!-- 하단 고정 버튼(작성자 글일 때만 노출) -->
     <div class="bottom-btn-group" v-if="isMyPost">
@@ -48,6 +50,52 @@ console.log(route.params);
 const { isLogin, user, checkLoginStatus } = useAuth(); // ② 로그인 상태 확인 함수 가져오기
 const isMyPost = ref(false); // 내가 올린 게시글인지 확인하는 변수
 const router = useRouter(); // 페이지 이동을 위한 라우터 객체(router와 구분 주의)
+const isApplied = ref(false); // 지원 여부 확인
+
+// 지원하기
+const handleApply = async () => {
+  console.log('지원하기 버튼 클릭');
+
+  // 유저 데이터에서 이름과 전화번호 가져오기(useAuth에는 해당정보 없고 별도의 테이블에 있음)
+  const { data, error } = await supabase
+    .from('user_info')
+    .select()
+    .eq('id', user.value.id)
+    .single();
+
+  if(error) {
+    alert(error.message || '오류가 발생했습니다.');
+    return;
+  }
+  console.log('user name,tel: ', data.name, data.tel);
+
+  // 지원내역 확인
+  if(isApplied.value) {
+    alert('이미 지원한 공고입니다.');
+    return;
+  } else {
+    // 지원내역 저장
+    const { error: err } = await supabase // 전달받는 error 변수명을 err로 변경
+      .from('job_apply_list')
+      .insert({
+        job_title: post.value.title,
+        employer_id: post.value.author, // 고용주: 글 작성자 ID
+        applicant_id: user.value.id, // 지원자: 현재 로그인한 사용자 ID
+        applicant_name: data.name, // 지원자: 현재 로그인한 사용자 이름
+        applicant_tel: data.tel, // 지원자: 현재 로그인한 사용자 전화번호
+        post_id: post.value.id, // 고용주가 게시한 글 ID
+      })
+      
+    if(err) {
+      alert(error.message || '오류가 발생했습니다.');
+      return;
+    } else {
+      alert('지원이 완료되었습니다.');
+      router.push('/job-list');
+    }
+  }
+
+};
 
 const handleDelete = async () => {
   // 삭제 확인 대화상자 표시
@@ -68,6 +116,27 @@ const handleDelete = async () => {
     }
 }
 
+// job_apply_list에 이미 지원한 내역이 있는지 확인
+const checkApply = async () => {
+  const { data: exist, error: existErr } = await supabase
+    .from('job_apply_list')
+    .select()
+    .eq('applicant_id', user.value.id) // 지원자: 현재 로그인한 사용자 ID로 조회
+    .eq('post_id', id); // 고용주가 게시한 글 ID로 조회
+
+  if(existErr) {
+    alert(existErr.message || '오류가 발생했습니다.');
+    return;
+  }
+  console.log('existErrt: ', existErr);
+
+  if(exist.length > 0) {
+    console.log(exist);
+    // alert('이미 지원한 내역이 있습니다.');
+    isApplied.value = true;
+  }
+}
+
 // 페이지 로딩 시 실행할 코드
 onMounted(async () => {
   await checkLoginStatus(); // 로그인 상태 확인
@@ -84,16 +153,19 @@ onMounted(async () => {
     post.value = data;
     console.log(post.value);
 
-    /// 내가 올린 게시글인지 확인
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    // 내가 올린 게시글인지 확인
     if(user.value.id === post.value.author) {
       isMyPost.value = true;
       console.log(user.value.id, post.value.author);
     }
 
-    if (error) {
-      alert(error.message);
-      return;
-    }
+    // 지원 여부 확인
+    await checkApply();
   }
 })
 
@@ -163,6 +235,10 @@ textarea {
 
   .btn-apply {
     background-color: var(--main-color-light);
+  }
+
+  .btn-disable {
+    background-color: rgba(0,0,0,0.5);
   }
 }
 </style>
